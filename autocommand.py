98bcdbbcec297ff1a8419fc277f25b4a286426ce
Faizan Zafar <faizanzafar619@gmail.com>
2023-03-29 13:52:13 -0700

    added comments of what is needed

--- autocommand.py
+++ autocommand.py
@@ -2,12 +2,18 @@ import os
 
 
 mailList =[]
-
+#read emails from text file separated by \n
 with open('maillist.txt') as f:
     for line in f:
         mailList.append(line.strip())
 
-# print(mailList)
+# get first name from email
 
+#update the mes.html file to include name
+
+#send emails to users in mailList using linux command
 for i in mailList:
-    os.system(' sendemail -xu faizan.zafar01@student.csulb.edu -xp YRrC8L37zgbWpdhv -s smtp-relay.sendinblue.com:587 -f es-records@csulb.edu -t '+i+' -u "Reminder: Update Your Emergency Contacts" -o message-header="From: CSULB Enrollment Services <es-records@csulb.edu>" -o message-header="Importance:High" -o message-content-type=html -o message-file=./msg.html')
+    os.system('sendemail -xu faizan.zafar01@student.csulb.edu -xp YRrC8L37zgbWpdhv -s smtp-relay.sendinblue.com:587 -f es-records@csulb.edu -t '+i+' -u "Reminder: Update Your Emergency Contacts" -o message-header="From: CSULB Enrollment Services <es-records@csulb.edu>" -o message-header="Importance:High" -o message-content-type=html -o message-file=./msg.html')
+
+
+#revert message file to go from name to {name}
