5794dc1561878ad95e63a7ab1fb90a4f7c52c009
Faizan Zafar <faizanzafar619@gmail.com>
2023-04-10 08:51:54 -0700

    comment out headless

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"')
 
-chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--headless')
 
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='/snap/bin/chromium.chromedriver')
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
