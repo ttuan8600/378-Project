856fda2881079e05ccb3df6b7f5775ff9418ce37
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2023-04-11 18:34:19 -0700

    fixed runmain to allow webinput

--- CSULB-Login/runmain.py
+++ CSULB-Login/runmain.py
@@ -1,11 +1,19 @@
 #this file will be what is called by the webserver(website) and will handle the input data.
 import sys
-
+import main
 #code from random website
 if __name__ == "__main__":
     print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-       with open("test.txt",'+a') as file:
-           file.write(arg)
+    main.login_email(sys.argv[1],sys.argv[2])
+    if sys.argv[3]:
+        main.call()
+    else:
+        main.text(sys.argv[1])
+    main.open_mycsulb()
+    main.log_info(sys.argv[1],sys.argv[2])
+    main.driver.close
+    # for i, arg in enumerate(sys.argv):
+    #    with open("test.txt",'+a') as file:
+    #        file.write(arg)
            
-        # print(f"Argument {i:>6}: {arg}")
+    #     # print(f"Argument {i:>6}: {arg}")
