5fd04930cb09481ed381eaa85f7becc50b8b62a6
Rushil <72721573+rushilp002@users.noreply.github.com>
2023-04-11 18:12:05 -0700

    Update app.js

--- CSULB-Login/app.js
+++ CSULB-Login/app.js
@@ -51,13 +51,11 @@ var emailInput = document.getElementsByName("email")[0];
 var passwordInput = document.getElementsByName("password")[0];
 
 document.getElementById("submitButton").addEventListener("click", function() {
-
   emailInput.value = "example@example.com";
   passwordInput.value = "password";
 
   document.forms[0].submit();
-}
-
+});
 
 
 
