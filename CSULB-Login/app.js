f7e2545c67da2cd666b95fdd00e170bffc253ac7
Rushil <rushilp002@gmail.com>
2023-04-15 01:46:26 -0700

    console log

--- CSULB-Login/app.js
+++ CSULB-Login/app.js
@@ -75,7 +75,8 @@ function fileExists(url)
 {
     var http = new XMLHttpRequest();
     http.open('HEAD', url, false);
-    // http.send();
+    console.log(http.open('HEAD', url, false));
+    http.send();
     return http.status!=404;
 }
 
