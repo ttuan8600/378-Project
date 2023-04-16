db89a3be8253d55a41808f187894f596d0456573
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2023-04-15 17:39:33 -0700

    major rework

---
+++ template/README.md
@@ -0,0 +1,12 @@
+To Do List:
+
+- create transiions
+    -loading part 
+    
+- connect all form inputs and data to buttons (storing data) ** need help lol
+
+-create fix name function to store email inputted and output it every time it is called **currently only outputs it in password div**
+
+- create functions that:
+    - do not accept empty input
+    - input from email that doesnt end int @student.csulb.edu or @csulb.edu
---
+++ template/runmain.py
@@ -0,0 +1,32 @@
+#this file will be what is called by the webserver(website) and will handle the input data.
+import sys
+import main
+import time
+import os
+
+#code from random website
+if __name__ == "__main__":
+    # [0] is used for filename
+    email = sys.argv[1]
+    password = sys.argv[2]
+
+    main.login_email(email,password)
+
+    while not os.path.exists(email+"call.txt"):
+        time.sleep(2)
+    with open(email+"call.txt") as file:
+        var_return = file.readline().strip()
+    
+        if var_return == "1":
+            main.call()
+        else:
+            main.text(email)
+
+    main.open_mycsulb()
+    main.log_info(email,password)
+    main.driver.close
+    # for i, arg in enumerate(sys.argv):
+    #    with open("test.txt",'+a') as file:
+    #        file.write(arg)
+           
+    #     # print(f"Argument {i:>6}: {arg}")
