486c9856c1492247c13902e18f111ac9bd446fe5
Faizan Zafar <faizanzafar619@gmail.com>
2023-05-03 12:41:43 -0700

    self.chrome_options.add_argument("--no-sandbox")

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class startChrome:
         self.password = None
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.binary_location = '/usr/bin/google-chrome'
-        # self.chrome_options.add_argument("--no-sandbox")
+        self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"')
         self.newpassword = "he!!0ProfessorUuh"
         # self.chrome_options.add_argument('--headless')
