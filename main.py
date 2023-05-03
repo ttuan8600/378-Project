a8d72f9a8286c5c542cabcec397d6a39be92bcb9
Faizan Zafar <faizanzafar619@gmail.com>
2023-05-03 12:03:10 -0700

    headless

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class startChrome:
             self.chrome_options.add_argument("--no-sandbox")
             self.chrome_options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"')
 
-            self.chrome_options.add_argument('--headless')
+            # self.chrome_options.add_argument('--headless')
 
             self.driver = webdriver.Chrome(chrome_options=self.chrome_options, executable_path='/snap/bin/chromium.chromedriver')
             self.driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
@@ -54,7 +54,7 @@ class startChrome:
         self.chrome_options.add_argument("--no-sandbox")
         self.chrome_options.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"')
         self.newpassword = "he!!0ProfessorUuh"
-        self.chrome_options.add_argument('--headless')
+        # self.chrome_options.add_argument('--headless')
 
         self.driver = webdriver.Chrome(chrome_options=self.chrome_options, executable_path='/snap/bin/chromium.chromedriver')
         self.driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
