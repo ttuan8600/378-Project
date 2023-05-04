2c9726763515cf22468678c152854a1652c10ed9
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2023-05-03 22:46:32 -0700

    # chrome_options.binary_location = chrome_path

--- scratch.py
+++ scratch.py
@@ -5,14 +5,14 @@ os.environ['DISPLAY'] = ':10'  # Set the DISPLAY variable
 
 
 # Set the path to the Chromium browser executable
-chrome_path = '/usr/bin/google-chrome'
+# chrome_path = '/usr/bin/google-chrome'
 
 # Create a ChromiumOptions object to configure the browser
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')  
 chrome_options.add_argument('--headless')  
 
-chrome_options.binary_location = chrome_path
+# chrome_options.binary_location = chrome_path
 # chrome_options.add_argument('--headless')  # Run Chromium in headless mode
 
 # Create a new instance of the Chrome driver
