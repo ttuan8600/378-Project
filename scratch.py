404d4c12b2c3a4c65d50eaef4b2e49914ca6b3e9
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2023-05-03 22:56:32 -0700

    driver.get_screenshot_as_file('screenshot.png')

--- scratch.py
+++ scratch.py
@@ -13,6 +13,7 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1920,1080')
 chrome_options.add_argument('--window-position=0,0')
 chrome_options.add_argument('--start-maximized')
+
 # chrome_options.add_argument('--headless')  
 
 # chrome_options.binary_location = chrome_path
@@ -24,7 +25,7 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 # Navigate to a web page
 driver.get('https://www.google.com')
 print(driver.title)
-
+driver.get_screenshot_as_file('screenshot.png')
 input()
 # Get the page title
 
