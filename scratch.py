067cdf29b1f8aa86a68398e3a9d28b874c3fbe82
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2023-05-03 22:55:49 -0700

    chrome_options.add_argument('--start-maximized')

--- scratch.py
+++ scratch.py
@@ -10,6 +10,9 @@ os.environ['DISPLAY'] = ':99'  # Set the DISPLAY variable
 # Create a ChromiumOptions object to configure the browser
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')  
+chrome_options.add_argument('--window-size=1920,1080')
+chrome_options.add_argument('--window-position=0,0')
+chrome_options.add_argument('--start-maximized')
 # chrome_options.add_argument('--headless')  
 
 # chrome_options.binary_location = chrome_path
@@ -21,6 +24,7 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 # Navigate to a web page
 driver.get('https://www.google.com')
 print(driver.title)
+
 input()
 # Get the page title
 
