77d9749bb9a5911f2f0f64ee10a82d80e7ae5282
Faizan Zafar <faizan.zafar01@student.csulb.edu>
2023-05-03 22:31:14 -0700

    input test

--- scratch.py
+++ scratch.py
@@ -4,17 +4,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox() #ask Faizan if this would work with the connection to the server?
-driver.get("#code from website, where we stored the data.")
-
+driver.get("google.com")
+input()
 #run the information got from website data?
-email_field = driver.find_element_by_name('email')
-password_field = driver.find_element_by_name('password')
-email_field.send_keys('# the email')
-password_field.send_keys('the password')
-password_field.send_keys(Keys.RETURN)
+# email_field = driver.find_element_by_name('email')
+# password_field = driver.find_element_by_name('password')
+# email_field.send_keys('# the email')
+# password_field.send_keys('the password')
+# password_field.send_keys(Keys.RETURN)
 
 time.sleep(5)
 
-subprocess.run(['python3', 'runmain.py', '-emailuser@example.com', '-passwordpassword'])
+# subprocess.run(['python3', 'runmain.py', '-emailuser@example.com', '-passwordpassword'])
 
 driver.quit()
